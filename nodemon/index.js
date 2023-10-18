c854e7e81854d876a3bbc279da14062bea7ea3dd
ammmar <ammaraslam7164@gmail.com>
2023-10-19 02:28:48 +0600

    page-routing-successfull

--- nodemon/index.js
+++ nodemon/index.js
@@ -1,37 +1,29 @@
 const http = require("http");
-const fs = require('fs')
+const fs = require("fs");
 const port = 3000;
 
 const hostname = "127.0.0.1";
 
 const server = http.createServer((req, res) => {
- 
-    if(req.url==='/'){
+  const handlereadFile = (filelocation, statuscode) => {
+    fs.readFile(filelocation, (err, data) => {
+      res.writeHead(statuscode, { "Content-Type": "text/html" });
 
-    fs.readFile('index.html', (err,data)=>{
+      res.write(data);
 
-        res.writeHead(200,{'Content-Type':'text/html'});
+      res.end();
+    });
+  };
 
-        res.write(data)
-
-        res.end();
-
-    })
-    }
-   else if(req.url==='/about'){
-
-        fs.readFile('about.html', (err,data)=>{
-    
-            res.writeHead(200,{'Content-Type':'text/html'});
-    
-            res.write(data)
-    
-            res.end();
-    
-        })
-        }
-   
-
+  if (req.url === "/") {
+    handlereadFile("index.html", 200);
+  } else if (req.url === "/about") {
+    handlereadFile("about.html", 200);
+  } else if (req.url === "/home") {
+    handlereadFile("home.html", 200);
+  } else {
+    handlereadFile("error.html", 404);
+  }
 });
 
 server.listen(port, hostname, () => {
