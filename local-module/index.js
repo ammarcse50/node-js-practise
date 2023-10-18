09693a3184b86cba9a135bcb3d5bbdf2ff8ed91d
ammmar <ammaraslam7164@gmail.com>
2023-10-18 22:15:22 +0600

    local-module-uploaded

---
+++ local-module/index.js
@@ -0,0 +1,13 @@
+//local method
+
+// method-1
+
+const imp =  require('./student');
+
+//  console.log(imp.getName())
+
+          // method-2
+
+const {getName,getAge} =  require('./student')
+
+  console.log(getName(),getAge())
---
+++ local-module/student.js
@@ -0,0 +1,35 @@
+
+
+
+const getName =()=>{
+    return "ammar aslam"
+}
+
+const getAge = () =>{
+
+
+    return "30yr"
+}
+
+
+ // export method-1
+
+// module.exports={
+
+//     getName,
+
+//     getAge
+// }
+
+
+// exports method-2
+
+// exports.getName =()=>{
+//     return "ammar aslam"
+// }
+
+
+// exports method-3
+
+//    exports.getName = getName;
+
