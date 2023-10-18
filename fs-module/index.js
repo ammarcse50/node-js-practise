9a44621d740bba8ac385278557ad7507c3f16998
ammmar <ammaraslam7164@gmail.com>
2023-10-18 22:54:38 +0600

    fs-module-done

---
+++ fs-module/index.js
@@ -0,0 +1,90 @@
+ // fs = file server
+
+const fs = require('fs')
+
+
+ // writeFile , appendFile 
+
+// fs.writeFile('demo1.txt','this is demo',(err)=>{
+
+//     if(err){
+//         console.log(err);
+//     }
+
+//     else{
+//     console.log('successfull',data)
+
+//     }
+
+
+
+
+// })
+
+
+// fs.readFile to read file.txt
+
+// fs.readFile('demo1.txt','utf-8',(err,data)=>{
+
+//     if(err){
+//         console.log(err);
+//     }
+
+//     else{
+//     console.log('successfull',data)
+
+//     }
+
+
+
+
+    //fs.rename to rename file.txt
+
+
+    // fs.rename('demo1.txt','demo2.txt',(err)=>{
+
+    //     if(err){
+    //         console.log(err);
+    //     }
+    
+    //     else{
+    //     console.log('successfull')
+    
+    //     }
+    // }
+    // )
+    
+    
+    // Delete anyhting  fs.unlink
+
+
+    //  fs.unlink('demo2.txt',(err)=>{
+
+    //     if(err){
+    //         console.log(err);
+    //     }
+    
+    //     else{
+    //     console.log('success')
+    
+    //     }
+    // }
+    // )
+
+    //fs.exists to check if file.txt exists
+
+
+    
+    // fs.exists('demo2.txt',(result)=>{
+
+    //     if(result){
+    //         console.log('found');
+    //     }
+    
+    //     else{
+    //     console.log('not found')
+    
+    //     }
+    // }
+    // )
+    
---
+++ os-path/index.js
@@ -0,0 +1,13 @@
+
+
+// os , path
+
+
+const os = require('os')
+
+
+// console.log(os)
+
+// console.log(os.userInfo())
+// console.log(os.homedir())
+console.log(os.totalmem())
