0d11809aa9f67b77e57b36c28e0a34cb40fff0b4
ammmar <ammaraslam7164@gmail.com>
2023-10-18 23:14:26 +0600

    join-path-os-up

--- os-path/index.js
+++ os-path/index.js
@@ -3,11 +3,23 @@
 // os , path
 
 
-const os = require('os')
+// const os = require('os')
 
 
-// console.log(os)
+// // console.log(os)
 
-// console.log(os.userInfo())
-// console.log(os.homedir())
-console.log(os.totalmem())
+// // console.log(os.userInfo())
+// // console.log(os.homedir())
+// console.log(os.totalmem())
+
+
+// console.log(__dirname)
+
+// console.log(__filename)
+
+
+const path = require('path')
+
+   const joinName = path.join(__dirname + "/views")
+
+   console.log(joinName)
